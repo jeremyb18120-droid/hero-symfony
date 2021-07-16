3e07f9680e2157433afbce6c0ed6d9e6c2fb743c
jeremyb18120-droid <84370941+jeremyb18120-droid@users.noreply.github.com>
2021-07-16 14:32:34 +0200

    des changements

---
+++ src/Services/HeroService.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Services;
+
+use App\Entity\Hero;
+
+class HeroService
+{
+    private $_listeHeros = [];
+
+    function __construct()
+    {
+        /**   $this->addHero(new Hero('Harley','MacKenzy','Harley Queen',true,'une psychiatre reconvertie en psychotique')); */
+       /**  $this->_listeHeros = [new Hero('Harley','MacKenzy','Harley Queen',true,'une psychiatre reconvertie en psychotique')];*/
+        /** array_push($_listeHeros,new Hero('Brice','Wayne','Batman',false,'une chauve souris avec une grosse super car')); */
+        $this->addHero(new Hero('Harley','MacKenzy','Harley Queen',true,'une psychiatre reconvertie en psychotique'));
+        $this->addHero(new Hero('Brice','Wayne','Batman',false,'une chauve souris avec une grosse super car'));
+        $this->addHero(new Hero('Clark','Kent','Superman',false,'Un extra-terrestre louche et journaliste'));
+        $this->addHero(new Hero('Diana','Prince','Wonderwoman',false,'une amazone qui roske du poney.'));
+    }
+    function getList()
+    {
+        return $this->_listeHeros;
+    }
+    function addHero($pHero)
+    {
+        array_push($this->_listeHeros,$pHero);
+    }
+}
